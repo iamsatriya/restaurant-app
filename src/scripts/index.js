a1d7910373c5c3d8aeb3a8db108250d145d83c91
Satriya <satriya.wicaksana888@gmail.com>
2021-03-05 20:41:32 +0700

    adding menu for phone

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,16 @@ import data from '../DATA.json';
 import '../scripts/component/restaurant-list.js';
 
 const restaurantList = document.querySelector('restaurant-list');
+const hamburgerMenu = document.querySelector('#hamburger-menu');
+const headerNav = document.querySelector('#header__nav');
+const closeNav = document.querySelector('#close-menu');
+
+hamburgerMenu.addEventListener('click', function() {
+  headerNav.classList.toggle('active');
+});
+closeNav.addEventListener('click', function() {
+  headerNav.classList.toggle('active');
+});
 
 const loadRestaurant = () => {
   restaurantList.restaurants = data.restaurants;
